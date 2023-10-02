8ddcd3db1f2e5f5dc6933ab1147e86a9100f5638
Mclean Kasambala <mcleankasambala@gmail.com>
2023-10-02 08:16:10 +0200

    added api files

---
+++ api/connection.php
@@ -0,0 +1,14 @@
+<?php
+// Create a database connection
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "pumani";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check the connection
+if ($conn->connect_error) {
+    http_response_code(500); // HTTP 500 Internal Server Error
+    die(json_encode(array("message" => "Connection failed: " . $conn->connect_error)));
+}
